1d5ed72b36996757fa2f6175ed944ec49ffb8404
yonekura <sizimiso0320@gmail.com>
2021-05-11 19:37:59 +0900

    リメンバーミー機能実装

--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -1,5 +1,56 @@
 RSpec.describe "UsersLogins", type: :request do
+  include SessionsHelper
+
   let(:user) { create(:user) }
+
+  def post_valid_information(remember_me = 0)
+    post login_path, params: {
+      session: {
+        email: user.email,
+        password: user.password,
+        remember_me: remember_me,
+      },
+    }
+  end
+
+  it "does not log out twice" do
+    get login_path
+    post_valid_information(0)
+    expect(is_logged_in?).to be_truthy
+    follow_redirect!
+    expect(request.fullpath).to eq user_path(user.id)
+    delete logout_path
+    expect(is_logged_in?).to be_falsey
+    follow_redirect!
+    expect(request.fullpath).to eq root_path
+    delete logout_path
+    follow_redirect!
+    expect(request.fullpath).to eq root_path
+  end
+
+  it "succeeds remember_token because of check remember_me" do
+    get login_path
+    post_valid_information(1)
+    expect(is_logged_in?).to be_truthy
+    expect(cookies[:remember_token]).not_to be_nil
+  end
+
+  it "has no remember_token because of check remember_me" do
+    get login_path
+    post_valid_information(0)
+    expect(is_logged_in?).to be_truthy
+    expect(cookies[:remember_token]).to be_nil
+  end
+
+  it "has no remember_token when users logged out and logged in" do
+    get login_path
+    post_valid_information(1)
+    expect(is_logged_in?).to be_truthy
+    expect(cookies[:remember_token]).not_to be_empty
+    delete logout_path
+    expect(is_logged_in?).to be_falsey
+    expect(cookies[:remember_token]).to be_empty
+  end
 
   describe "GET /login" do
     context "invalid information" do
