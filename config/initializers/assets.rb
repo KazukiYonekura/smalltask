b63d06dd7dce4aac45749b88ddd905ef811b0f5b
yonekura <sizimiso0320@gmail.com>
2021-06-05 22:04:33 +0900

    assets設定変更

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,9 @@ Rails.application.config.assets.precompile += %w( application.css )
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
+Rails.application.config.assets.configure do |env|
+  env.export_concurrent = false
+end
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
